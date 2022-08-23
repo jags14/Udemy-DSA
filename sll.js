858d055d271814b460b40edce727f15df5274328
Jagpat Singh <jsingh14192@gmail.com>
2022-08-23 21:10:17 +0530

    Adding push and pop methods to linkedList

--- sll.js
+++ sll.js
@@ -41,4 +41,29 @@ class LinkedList {
         this.size++;
         return this;
     }
+
+    pop(){
+        if(!this.head) return undefined;
+        var current = this.head;
+        var pre = current;
+        while(current.next){
+            pre = current;
+            current = current.next;
+        }
+        
+        this.tail = pre;
+        this.tail.next = null;
+        this.size--;
+        return current;
+    }
+
+    shift(){
+        if(!this.head) return undefined;
+        var currentHead = this.head;
+        this.head = this.head.next;
+        currentHead.next = null;
+        this.size--;
+        return currentHead;
+        
+    }
 }
